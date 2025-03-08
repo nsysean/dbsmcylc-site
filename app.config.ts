24ef6d70090ebe46601003b29756d33f0f6409e2
nsysean <nsysean2@gmail.com>
2025-03-09 03:04:46 +0800

    add cloudflare compatibility

--- app.config.ts
+++ app.config.ts
@@ -15,4 +15,12 @@ export default defineConfig({
   },
   ssr: true,
   extensions: ["mdx", "md"],
+  server: {
+    preset: "cloudflare-pages",
+    // We will need to enable CF Pages node compatiblity
+    // https://developers.cloudflare.com/workers/runtime-apis/nodejs/asynclocalstorage/
+    rollupConfig: {
+      external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
+    },
+  },
 });
