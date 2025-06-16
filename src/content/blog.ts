ee762a9e5ba63d38d5afcce97711b04f2fb9137c
nsysean <76026698+nsysean@users.noreply.github.com>
2025-06-16 20:09:42 +0800

    refresh content

--- src/content/blog.ts
+++ src/content/blog.ts
@@ -1,6 +1,9 @@
 import * as blog2 from "../routes/blog/adoption-in-literature.mdx";
+import * as blog3 from "../routes/blog/adoption-in-history.mdx";
 import imageBlog2 from "../../public/blog2-hero.jpg?responsive";
+import imageBlog3 from "../../public/blog3-hero.jpg?responsive";
 
 export default [
   { ...blog2, image: imageBlog2 },
+  { ...blog3, image: imageBlog3 }
 ];
